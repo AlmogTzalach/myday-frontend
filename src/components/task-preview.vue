731e707a87bbf03a30939bd15a9b0d805ed3a512
Anton Kandyba <antonkandyba@gmail.com>
2022-07-20 15:15:54 +0300

    added task data

--- src/components/task-preview.vue
+++ src/components/task-preview.vue
@@ -2,6 +2,13 @@
 	<div class="flex">
 		<p>{{ task.id }}</p>
 		<p>{{ task.title }}</p>
+		<p>{{ task.statusId }}</p>
+		<p>{{ task.priorityId }}</p>
+		<p>People</p>
+		<p>{{ task.date }}</p>
+		<p>{{ task.checkbox }}</p>
+		<p>{{ task.link }}</p>
+		<p>{{ task.txt }}</p>
 	</div>
 </template>
 
