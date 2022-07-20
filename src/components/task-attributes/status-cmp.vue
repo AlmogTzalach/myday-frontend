b5dd351a07721dd80dd95008506c37b0d086edf2
Almog Tzalach <almogtza@gmail.com>
2022-07-20 23:34:12 +0300

    added status-cmp for tasks

---
+++ src/components/task-attributes/status-cmp.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="attr-container">
+        <div class="label" :style="{ 'background-color': labelColor }">
+            <p>{{ status.title }}</p>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "statusCmp",
+    prop: {
+        statusId: String,
+    },
+    data() {
+        return {}
+    },
+    computed: {
+        labelColor() {
+
+        }
+    },
+}
+</script>
