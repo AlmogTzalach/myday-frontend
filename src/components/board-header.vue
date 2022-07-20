b63929701434b8d27490ece95156bf28eb9534a9
Lior Abir <liorabir909@gmail.com>
2022-07-20 18:16:36 +0300

    header css

--- src/components/board-header.vue
+++ src/components/board-header.vue
@@ -1,7 +1,44 @@
 <template>
-	<div>Board Header</div>
+	<header class="board-header">
+		<div class="board-actions flex">
+			<div class="title-input flex">
+				<input type="text" v-model="board.title" contenteditable />
+				<!-- <button>star</button>
+				<button>info</button> -->
+			</div>
+			<div class="btns flex">
+				<button @click="addMember()" class="btn add-member-btn">
+					+ <span>invite / {{ board.members.length }}</span>
+				</button>
+			</div>
+		</div>
+
+		<div class="board-vue flex">
+			<div class="btn-container">
+				<button @click="setTableVue" class="btn vue-btn">
+					Main Table
+				</button>
+				<div></div>
+			</div>
+			<span>|</span>
+			<button class="btn add-vue-btn">+</button>
+		</div>
+	</header>
 </template>
 
 <script>
-	export default {}
+export default {
+	name: "board-header",
+	props: {
+		board: Object,
+	},
+	methods: {
+		addMember() {
+			console.log("add memeber")
+		},
+		setTableVue() {
+			console.log("set vue")
+		},
+	},
+}
 </script>
