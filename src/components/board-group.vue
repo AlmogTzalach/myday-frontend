2581fb1c87ab01e350db798d58a0790c02ad1eab
Anton Kandyba <antonkandyba@gmail.com>
2022-07-21 12:45:43 +0300

    fixed left indicator

--- src/components/board-group.vue
+++ src/components/board-group.vue
@@ -2,24 +2,26 @@
 	<section class="board-group">
 		<h4 class="group-title">{{ group.title }}</h4>
 
-		<div class="group-header-row task-row grid">
-			<div class="task-name task-title">
-				<p>Task</p>
+		<div class="group-table">
+			<div class="group-header-row task-row grid">
+				<div class="task-name task-title">
+					<p>Task</p>
+				</div>
+				<div class="task-data grid">
+					<p>Status</p>
+					<p>Priority</p>
+					<p>People</p>
+					<p>Date</p>
+					<p>Checkbox</p>
+					<p>Link</p>
+					<p>Text</p>
+					<p class="empty-data"></p>
+				</div>
 			</div>
-			<div class="task-data grid">
-				<p>Status</p>
-				<p>Priority</p>
-				<p>People</p>
-				<p>Date</p>
-				<p>Checkbox</p>
-				<p>Link</p>
-				<p>Text</p>
-				<p class="empty-data"></p>
-			</div>
-		</div>
 
-		<div v-for="task in group.tasks" :key="task.id">
-			<task-preview :task="task" :currGroup="group.id" />
+			<div v-for="task in group.tasks" :key="task.id">
+				<task-preview :task="task" :currGroup="group.id" />
+			</div>
 		</div>
 	</section>
 </template>
