5f8a451934593bec95df28d276756a6bc0387943
Anton Kandyba <antonkandyba@gmail.com>
2022-07-21 20:00:46 +0300

    fixed immediate line

--- src/views/board-app.vue
+++ src/views/board-app.vue
@@ -25,62 +25,63 @@
 </template>
 
 <script>
-import mainNav from '@/components/main-nav.vue'
-import boardsNav from '@/components/boards-nav.vue'
-import boardHeader from '@/components/board-header.vue'
-import boardFilter from '@/components/board-filter.vue'
-import boardGroup from '@/components/board-group.vue'
-import taskDetails from '@/components/task-details.vue'
+	import mainNav from '@/components/main-nav.vue'
+	import boardsNav from '@/components/boards-nav.vue'
+	import boardHeader from '@/components/board-header.vue'
+	import boardFilter from '@/components/board-filter.vue'
+	import boardGroup from '@/components/board-group.vue'
+	import taskDetails from '@/components/task-details.vue'
 
-export default {
-	name: 'board-app',
+	export default {
+		name: 'board-app',
 
-	data() {
-		return {}
-	},
-
-	methods: {
-		addTask() {
-			const firstGroupId = this.currBoard.groups[0].id
-			this.$store.dispatch({
-				type: 'addTask',
-				groupId: firstGroupId,
-				name: 'New Task',
-			})
+		data() {
+			return {}
 		},
 
-		onTaskMoved(newGroup) {
-			const boardCopy = JSON.parse(JSON.stringify(this.currBoard))
-			let idx = boardCopy.groups.findIndex((group) => group.id === newGroup.id)
-			boardCopy.groups.splice(idx, 1, newGroup)
-
-			this.$store.dispatch({ type: 'saveBoard', newBoard: boardCopy })
-		},
-	},
-
-	computed: {
-		currBoard() {
-			return this.$store.getters.currBoard
-		},
-	},
-
-	watch: {
-		'$route.params.boardId': {
-			handler(boardId) {
-				console.log(boardId)
+		methods: {
+			addTask() {
+				const firstGroupId = this.currBoard.groups[0].id
+				this.$store.dispatch({
+					type: 'addTask',
+					groupId: firstGroupId,
+					name: 'New Task',
+				})
 			},
-			immediate: true,
-		},
-		immediate: true,
-	},
 
-	components: {
-		mainNav,
-		boardHeader,
-		boardFilter,
-		boardGroup,
-		taskDetails,
-		boardsNav,
-	},
-}
+			onTaskMoved(newGroup) {
+				const boardCopy = JSON.parse(JSON.stringify(this.currBoard))
+				let idx = boardCopy.groups.findIndex(
+					(group) => group.id === newGroup.id
+				)
+				boardCopy.groups.splice(idx, 1, newGroup)
+
+				this.$store.dispatch({ type: 'saveBoard', newBoard: boardCopy })
+			},
+		},
+
+		computed: {
+			currBoard() {
+				return this.$store.getters.currBoard
+			},
+		},
+
+		watch: {
+			'$route.params.boardId': {
+				handler(boardId) {
+					console.log(boardId)
+				},
+				immediate: true,
+			},
+		},
+
+		components: {
+			mainNav,
+			boardHeader,
+			boardFilter,
+			boardGroup,
+			taskDetails,
+			boardsNav,
+		},
+	}
 </script>
