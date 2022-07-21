1f74f2ea3dbb85a63f489b8435298957df68305f
Almog Tzalach <almogtza@gmail.com>
2022-07-21 09:36:35 +0300

    added date-cmp and people-cmp

---
+++ src/components/task-attributes/date-cmp.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="attr-container">
+        <input type="date" v-model="dateInput">
+    </section>
+</template>
+
+<script>
+export default {
+    name: "dateCmp",
+
+    props: {
+        date: Number,
+    },
+
+    data() {
+        return {
+            dateInput: this.date
+        }
+    },
+
+    computed: {
+        formattedDate() {
+            return `${this.date.getDate()}/${this.date.getMonth()}/${this.date.getFullYear()}/`
+        },
+    },
+}
+</script>
