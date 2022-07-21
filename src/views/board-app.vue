cd31126e13e708f68a5f12498a2223edf263898b
Anton Kandyba <antonkandyba@gmail.com>
2022-07-21 19:49:28 +0300

    merge conflict fixed

--- src/views/board-app.vue
+++ src/views/board-app.vue
@@ -8,9 +8,7 @@
 
 			<div class="flex main-actions-container">
 				<div class="add-btns">
-					<button class="btn1 add-task-btn" @click="addTask">
-						New Task
-					</button>
+					<button class="btn1 add-task-btn" @click="addTask">New Task</button>
 					<button class="btn2 add-task-btn">></button>
 				</div>
 
@@ -24,28 +22,18 @@
 </template>
 
 <script>
-import mainNav from '@/components/main-nav.vue'
-import boardsNav from '@/components/boards-nav.vue'
-import boardHeader from '@/components/board-header.vue'
-import boardFilter from '@/components/board-filter.vue'
-import boardGroup from '@/components/board-group.vue'
-import taskDetails from '@/components/task-details.vue'
+	import mainNav from '@/components/main-nav.vue'
+	import boardsNav from '@/components/boards-nav.vue'
+	import boardHeader from '@/components/board-header.vue'
+	import boardFilter from '@/components/board-filter.vue'
+	import boardGroup from '@/components/board-group.vue'
+	import taskDetails from '@/components/task-details.vue'
 
-export default {
-	name: 'board-app',
+	export default {
+		name: 'board-app',
 
-	data() {
-		return {}
-	},
-
-	methods: {
-		addTask() {
-			const firstGroupId = this.currBoard.groups[0].id
-			this.$store.dispatch({
-				type: 'addTask',
-				groupId: firstGroupId,
-				name: 'New Task',
-			})
+		data() {
+			return {}
 		},
 
 		methods: {
@@ -57,16 +45,16 @@ export default {
 					name: 'New Task',
 				})
 			},
+
 			onTaskMoved(newGroup) {
 				const boardCopy = JSON.parse(JSON.stringify(this.currBoard))
-				let idx = boardCopy.groups.findIndex(
-					(group) => group.id === newGroup.id
-				)
+				let idx = boardCopy.groups.findIndex((group) => group.id === newGroup.id)
 				boardCopy.groups.splice(idx, 1, newGroup)
 
 				this.$store.dispatch({ type: 'saveBoard', newBoard: boardCopy })
 			},
 		},
+
 		computed: {
 			currBoard() {
 				return this.$store.getters.currBoard
@@ -82,15 +70,14 @@ export default {
 			},
 			immediate: true,
 		},
-	},
 
-	components: {
-		mainNav,
-		boardHeader,
-		boardFilter,
-		boardGroup,
-		taskDetails,
-		boardsNav,
-	},
-}
+		components: {
+			mainNav,
+			boardHeader,
+			boardFilter,
+			boardGroup,
+			taskDetails,
+			boardsNav,
+		},
+	}
 </script>
