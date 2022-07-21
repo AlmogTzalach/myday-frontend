eba59cc08cd621369e763545f9e4f7f9fcd662b0
Lior Abir <liorabir909@gmail.com>
2022-07-21 22:24:17 +0300

    filter btns css

--- src/components/board-filter.vue
+++ src/components/board-filter.vue
@@ -2,20 +2,28 @@
 	<section class="board-filter">
 		<div class="search-filter-container">
 			<button class="btn search-btn" contenteditable>
-				<el-icon><Search /></el-icon>
+				<img src="../assets/icons/search.svg" alt="" />
 				<span>Search</span>
 			</button>
 		</div>
 
 		<button class="btn person-btn">
-			<el-icon><User /></el-icon>
+			<img src="../assets/icons/person-round.svg" alt="" />
 			<span>Person</span>
 		</button>
 
-		<button class="btn">
-			<el-icon><Filter /></el-icon>
-			<span> Filter </span>
+		<!-- <button class="btn"> -->
+		<button>
+			<div class="btn1 flex">
+				<img src="../assets/icons/filter.svg" alt="" />
+				<span> Filter </span>
+			</div>
+
+			<div class="btn2">
+				<img src="../assets/icons/down.svg" alt="" />
+			</div>
 		</button>
+		<!-- </button> -->
 
 		<button class="btn">
 			&#8645;
