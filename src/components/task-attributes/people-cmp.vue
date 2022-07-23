7b07d5d4cca87c32766cd5bd55e6c2b46966727e
Almog Tzalach <almogtza@gmail.com>
2022-07-23 23:30:35 +0300

    added people cmp modal

--- src/components/task-attributes/people-cmp.vue
+++ src/components/task-attributes/people-cmp.vue
@@ -1,8 +1,24 @@
 <template>
     <section class="attr-container">
-                <div v-for="person in people" :key="person.id" class="flex align-center">
-                    <el-avatar :size="30" :src="person.imgUrl" :fit="'fill'" :title="person.fullName" />
+        <el-popover :placement="modalPlacement" :width="200" trigger="click" @show="changePlacement">
+            <template #reference>
+                <div class="avatar-container flex align-center">
+                    <div v-if="people.length" v-for="person in people" :key="person.id">
+                        <el-avatar class="avatar" :size="30" :src="person.imgUrl" :fit="'fill'"
+                            :title="person.fullName" />
+                    </div>
+                    <div v-else>
+                        <img class="empty-avatar" src="../../assets/icons/person-round.svg">
+                    </div>
                 </div>
+            </template>
+            <div class="avatar-container">
+                <div v-for="user in users" :key="user.id" class="flex align-center">
+                    <el-avatar :size="30" :src="user.imgUrl" :fit="'fill'" :title="user.fullName" />
+                    <span>{{ user.fullName }}</span>
+                </div>
+            </div>
+        </el-popover>
     </section>
 </template>
 
@@ -39,9 +55,12 @@ export default {
 }
 </script>
  <style>
- .avatar{
-    width: 30px;
-    height: 30px;
-    border-radius: 50%;
+ .empty-avatar {
+     width: 35px;
  }
+ 
+ .avatar {
+     border: 2px solid #f5f6f8;
+ }
+ 
  </style>
