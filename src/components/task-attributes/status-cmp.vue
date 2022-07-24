31e35783fe6314443e7ecee35245046c43bcdd54
Almog Tzalach <almogtza@gmail.com>
2022-07-24 15:49:11 +0300

    added close label picker on select

--- src/components/task-attributes/status-cmp.vue
+++ src/components/task-attributes/status-cmp.vue
@@ -1,18 +1,19 @@
 <template>
-    <el-popover :placement="modalPlacement" :width="200" trigger="click" @show="changePlacement">
+    <el-popover v-model:visible="visible" :placement="modalPlacement" :width="200" trigger="click">
         <template #reference>
-            <section class="attr-container" :style="{ 'background-color': status.color }">
+            <section class="attr-container" :style="{ 'background-color': status.color }" @click="visible = true" v-click-outside.stop="onClose">
                 <div class="label">
                     <p>{{ status.title }}</p>
                 </div>
             </section>
         </template>
-        <label-picker :labels="this.statusLabels" @labelPicked="changeStatus"></label-picker>
+        <label-picker :labels="this.statusLabels" @labelPicked="changeStatus" @click="visible = false"></label-picker>
     </el-popover>
 </template>
 
 <script>
 import labelPicker from '../label-picker.vue'
+import { ref } from 'vue'
 
 export default {
     name: "statusCmp",
@@ -23,11 +24,16 @@ export default {
 
     data() {
         return {
-            modalPlacement: 'bottom'
+            modalPlacement: 'bottom',
+            visible: ref(false)
         }
     },
 
     methods: {
+        onClose() {
+            console.log('test');
+            this.visible = false
+        },
         changePlacement(ev) {
             const vpH = window.innerHeight
             this.modalPlacement = ev.clientY > (vpH / 2) ? 'top' : 'bottom'
