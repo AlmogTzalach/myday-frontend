1f1e132136ef545f980a60af39fdc0ffdba74ac7
Anton Kandyba <antonkandyba@gmail.com>
2022-07-25 12:47:03 +0300

    scroll layout half-fixed

--- src/components/task-preview.vue
+++ src/components/task-preview.vue
@@ -22,28 +22,30 @@
 			</div>
 		</el-popover>
 		<div class="task-row grid">
-			<div
-				class="task-name name-cell grid"
-				:style="{ 'border-left-color': group.style.color }"
-			>
+			<div class="task-name name-cell grid">
+				<!-- :style="{ 'border-left-color': group.style.color }" -->
+				<div class="empty-left"></div>
+				<!-- <div class="left-indicator"></div> -->
 				<div class="side-border"></div>
-				<div class="edit-title">
-					<p contenteditable @input="onTitleUpdate" class="task-title">
-						{{ currTask.title }}
-					</p>
-				</div>
-				<div class="conversation-img" @click="onTaskChat">
-					<el-tooltip
-						effect="dark"
-						content="Start conversetion"
-						placement="top"
-						class="el-title"
-					>
-						<img
-							src="../assets/icons/add-update.svg"
-							alt="conversation"
-						/>
-					</el-tooltip>
+				<div class="task-title-wrapper flex space-between">
+					<div class="edit-title flex align-center">
+						<p contenteditable @input="onTitleUpdate" class="task-title">
+							{{ currTask.title }}
+						</p>
+					</div>
+					<div class="conversation-img" @click="onTaskChat">
+						<el-tooltip
+							effect="dark"
+							content="Start conversetion"
+							placement="top"
+							class="el-title"
+						>
+							<img
+								src="../assets/icons/add-update.svg"
+								alt="conversation"
+							/>
+						</el-tooltip>
+					</div>
 				</div>
 			</div>
 			<div class="task-data grid">
@@ -61,77 +63,77 @@
 </template>
 
 <script>
-import statusCmp from './task-attributes/status-cmp.vue'
-import priorityCmp from './task-attributes/priority-cmp.vue'
-import peopleCmp from './task-attributes/people-cmp.vue'
-import dateCmp from './task-attributes/date-cmp.vue'
-import checkboxCmp from './task-attributes/checkbox-cmp.vue'
-import linkCmp from './task-attributes/link-cmp.vue'
-import textCmp from './task-attributes/text-cmp.vue'
-import taskDetails from '@/components/task-details.vue'
+	import statusCmp from './task-attributes/status-cmp.vue'
+	import priorityCmp from './task-attributes/priority-cmp.vue'
+	import peopleCmp from './task-attributes/people-cmp.vue'
+	import dateCmp from './task-attributes/date-cmp.vue'
+	import checkboxCmp from './task-attributes/checkbox-cmp.vue'
+	import linkCmp from './task-attributes/link-cmp.vue'
+	import textCmp from './task-attributes/text-cmp.vue'
+	import taskDetails from '@/components/task-details.vue'
 
-export default {
-	name: 'taskPreview',
+	export default {
+		name: 'taskPreview',
 
-	props: {
-		task: Object,
-		group: Object,
-	},
-
-	data() {
-		return {
-			currTask: null,
-			isChatShown: false,
-		}
-	},
-
-	computed: {
-		cmpsOrder() {
-			return this.$store.getters.cmpsOrder
+		props: {
+			task: Object,
+			group: Object,
 		},
-	},
 
-	methods: {
-		onTaskChat() {
-			this.isChatShown = !this.isChatShown
+		data() {
+			return {
+				currTask: null,
+				isChatShown: false,
+			}
 		},
-		onDelete() {
-			this.$store.dispatch({
-				type: 'removeTask',
-				groupId: this.group.id,
-				taskId: this.task.id,
-			})
-		},
-		onTaskUpdate() {
-			this.$store.dispatch({
-				type: 'updateTask',
-				groupId: this.group.id,
-				newTask: JSON.parse(JSON.stringify(this.currTask)),
-			})
-		},
-		onTitleUpdate(ev) {
-			this.currTask.title = ev.target.innerText
-			this.onTaskUpdate()
-		},
-		updateData(newTask) {
-			this.currTask = newTask
-			this.onTaskUpdate()
-		},
-	},
 
-	created() {
-		this.currTask = JSON.parse(JSON.stringify(this.task))
-	},
+		computed: {
+			cmpsOrder() {
+				return this.$store.getters.cmpsOrder
+			},
+		},
 
-	components: {
-		taskDetails,
-		statusCmp,
-		priorityCmp,
-		peopleCmp,
-		dateCmp,
-		checkboxCmp,
-		linkCmp,
-		textCmp,
-	},
-}
+		methods: {
+			onTaskChat() {
+				this.isChatShown = !this.isChatShown
+			},
+			onDelete() {
+				this.$store.dispatch({
+					type: 'removeTask',
+					groupId: this.group.id,
+					taskId: this.task.id,
+				})
+			},
+			onTaskUpdate() {
+				this.$store.dispatch({
+					type: 'updateTask',
+					groupId: this.group.id,
+					newTask: JSON.parse(JSON.stringify(this.currTask)),
+				})
+			},
+			onTitleUpdate(ev) {
+				this.currTask.title = ev.target.innerText
+				this.onTaskUpdate()
+			},
+			updateData(newTask) {
+				this.currTask = newTask
+				this.onTaskUpdate()
+			},
+		},
+
+		created() {
+			this.currTask = JSON.parse(JSON.stringify(this.task))
+		},
+
+		components: {
+			taskDetails,
+			statusCmp,
+			priorityCmp,
+			peopleCmp,
+			dateCmp,
+			checkboxCmp,
+			linkCmp,
+			textCmp,
+		},
+	}
 </script>
