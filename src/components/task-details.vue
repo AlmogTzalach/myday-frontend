c088c79ef9b297639fa9ef85d817358e4802dac2
Anton Kandyba <antonkandyba@gmail.com>
2022-07-26 13:59:33 +0300

    changed drawer  size

--- src/components/task-details.vue
+++ src/components/task-details.vue
@@ -2,7 +2,7 @@
 	<!-- <el-button @click="visible = true">
 		Open Drawer with customized header
 	</el-button> -->
-	<el-drawer v-model="visible" :show-close="true">
+	<el-drawer v-model="visible" :show-close="true" :size="700">
 		<template #header="{ close, titleId, titleClass }">
 			<h4 :id="titleId" :class="titleClass">This is a custom header!</h4>
 		</template>
