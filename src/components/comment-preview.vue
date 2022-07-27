e8d13da4836c8ddca740ad1a67f4ab11e1dc911a
Almog Tzalach <almogtza@gmail.com>
2022-07-27 19:19:00 +0300

    added comment section

---
+++ src/components/comment-preview.vue
@@ -0,0 +1,21 @@
+<template >
+    <section>
+        <div class="comment-container">
+            <div class="comment-avatar flex align-center">
+                <img class="avatar" :src="comment.byMember.imgUrl">
+                <p class="user-name">{{ comment.byMember.fullName }}</p>
+            </div>
+            <p class="comment-text">{{ comment.txt }}</p>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    name: 'commentPreview',
+    props: {
+        comment: Object,
+    },
+}
+</script>
+<style >
+</style>
