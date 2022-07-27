a86ff5d087fb2b27cba11d481fafc5b233d6a81f
Almog Tzalach <almogtza@gmail.com>
2022-07-27 22:57:12 +0300

    added sign up and login pages

---
+++ src/views/login.vue
@@ -0,0 +1,41 @@
+<template>
+    <header class="login-header flex space-between align-center">
+        <router-link to="/">
+            <div class="logo-container flex align-center">
+                <img src="../assets/monday_logo_icon.png">
+                <h1 class="logo-title">myday</h1>
+            </div>
+        </router-link>
+    </header>
+    <section class="login-page-container flex column align-center">
+        <h2>Log in to your account</h2>
+        <el-form class="flex column align-center">
+            <el-input v-model="userName" placeholder="example@company.com" size="large" />
+            <el-input v-model="password" placeholder="Enter your password" size="large" type="password"
+                autocomplete="off" />
+            <el-button type="primary" @submit="onLogin">Log in</el-button>
+        </el-form>
+        <p>Don't have an account yet? <router-link to="/signup">Sign up</router-link></p>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'loginPage',
+    data() {
+        return {
+            userName: null,
+            password: null
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+}
+</script>
+
+<style >
+</style>
